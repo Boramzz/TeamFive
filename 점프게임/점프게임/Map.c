a42f755e7fcc9710b8b64b5c5ff6905f08e3088e
Hwangtaehun <r283@naver.com>
2022-11-17 20:52:14 +0900

    랭킹창 spacebar누르면 게임 종료 추가

--- 점프게임/점프게임/Map.c
+++ 점프게임/점프게임/Map.c
@@ -97,6 +97,58 @@ void mapping(int map[size_y][size_x], int* score)
 	}*/
 }
 
+int rankscreen(int map[size_y][size_x], int* score)
+{
+	int key;
+	RANK rank[ARR_SIZE];
+	FILE* inf;
+	if ((fopen_s(&inf, "Rank.txt", "rb")) != 0)
+	{
+		printf("파일 오픈 실패\n");
+		exit(0);
+	}
+
+
+	for (int i = 0; i < ARR_SIZE; i++)
+	{
+		fscanf(inf, "%s %d %d\n", rank[i].name, &rank[i].score, &rank[i].time);
+	}
+
+	fclose(inf);
+
+	system("cls");
+	printf("\n\t\tRanking\n\n");
+	printf("\t등수 이름        점수  시간\n");
+
+	for (int i = 0; i < ARR_SIZE; i++)
+	{
+		printf("\n\t%2d등 %-10s %5d %5d\n", i + 1, rank[i].name, rank[i].score, rank[i].time);
+	}
+
+	printf("\n");
+	printf("\n    enter을 누르면 게임 다시 시작\n");
+	printf("    spacebar를 누르면 게임이 종료됩니다.\n");
+	while (1)
+	{
+		if (_kbhit())
+		{
+			key = _getch();
+			if (key == 13)
+				break;
+			else if (key == 32)
+			{
+				system("cls");
+				return 0;
+			}
+				
+		}
+	}
+	system("cls");
+	mapbase(map);
+	mapping(map, score);
+	return 1;
+}
+
 int getKeyDown()
 {
 	if (_kbhit() != 0)
@@ -104,4 +156,13 @@ int getKeyDown()
 		return _getch();
 	}
 	return 0;
+}
+
+void print(RANK arr[])/*삭제*/
+{
+	for (int i = 0; i < ARR_SIZE; i++)
+	{
+		printf("%s, %d, %d\n", arr->name, arr->score, arr->time);
+		arr++;
+	}
 }
--- 점프게임/점프게임/Map.h
+++ 점프게임/점프게임/Map.h
@@ -10,7 +10,16 @@
 #define size_y 10
 #define c_y size_y-2 //내 위치 
 #define c_x 2
+#define ARR_SIZE 5
+
+typedef struct tag_rank {
+	char name[10];
+	int score;
+	int time;
+}RANK;
 
 void mapbase(int map[size_y][size_x]);
 void mapping(int map[size_y][size_x], int* score);
+int rankscreen(int map[size_y][size_x], int* score);
 int getKeyDown();
+void print(RANK arr[]); // 삭제하기
