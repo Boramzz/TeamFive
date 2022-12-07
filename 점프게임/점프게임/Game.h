2ef8d6fcd2d8ccf7a81f597074c3b515ed7a959d
Hwangtaehun <r283@naver.com>
2022-12-07 19:43:45 +0900

    Update Game.h
    해더 파일에 적는 이유는 재활용도 있지만 무슨 함수를 적었는지 알기위해서 필요합니다. 그래서 해더파일 추가해서 올립니다.

--- 점프게임/점프게임/Game.h
+++ 점프게임/점프게임/Game.h
@@ -2,6 +2,7 @@
 #include "Map.h"
 
 void jump(int map[size_y][size_x], int* score, int a, double* timer);
+void itemmaker(int map[size_y][size_x]);
 void obstaclemaker(int map[size_y][size_x]);
 void obstaclemove(int map[size_y][size_x], int* speed, int* score, int* otime, double* timer);
 int gameover(int map[size_y][size_x], int* score, double* timer);
